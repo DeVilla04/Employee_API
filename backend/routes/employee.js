c188c74c6c5f03cb2ef7e9ee263f77ab1055849a
DeVilla04 <dalalvraj21@gmail.com>
2023-04-14 02:07:37 +0530

    Updated the routes

--- backend/routes/employee.js
+++ backend/routes/employee.js
@@ -8,10 +8,10 @@ import {
 } from "../controller/employee.js";
 const router = express.Router();
 
-router.get("/all", getEmployees);
-router.get("/:id", getEmployee);
-router.post("/create", createEmployee);
-router.put("/update/:id", updateEmployee);
-router.delete("/delete/:id", deleteEmployee);
+router.get("/all", getEmployees); // List all the employees with pagination
+router.get("/:id", getEmployee); // Get a single employee using id
+router.post("/create", createEmployee); // Create a new employee
+router.put("/update/:id", updateEmployee); // Update an employee using id
+router.delete("/delete/:id", deleteEmployee); // Delete an employee using id
 
 export default router;
